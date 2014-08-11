5b738e50aaccb7338e5b41589725bc371364cd70
Max Woolf <maxehmookau@users.noreply.github.com>
2014-08-11 11:14:17 +0100

    Update autotest.sh

--- autotest.sh
+++ autotest.sh
@@ -1,3 +1,5 @@
+# You'll need to install watchdog
+# [sudo] pip install watchdog
 watchmedo shell-command --patterns="*.h;*.m" \
                         --recursive \
                         --command="xcodebuild test -workspace NymbolKit.xcworkspace -scheme NymbolKit -sdk iphonesimulator7.1 | xcpretty -c"\
