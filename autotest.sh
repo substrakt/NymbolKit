9367301d56734e71b17688f84bc3f59f53287261
Max Woolf <maxehmookau@users.noreply.github.com>
2014-08-11 11:13:50 +0100

    Prettify autotest script

--- autotest.sh
+++ autotest.sh
@@ -1,1 +1,4 @@
-watchmedo shell-command --patterns="*.h;*.m" --recursive --command="xcodebuild test -workspace NymbolKit.xcworkspace -scheme NymbolKit -sdk iphonesimulator7.1 | xcpretty -c" .
+watchmedo shell-command --patterns="*.h;*.m" \
+                        --recursive \
+                        --command="xcodebuild test -workspace NymbolKit.xcworkspace -scheme NymbolKit -sdk iphonesimulator7.1 | xcpretty -c"\
+                        .
